54843eb0d4608373edf256ba1b7e062e7bb69c9c
CarathrynBeutel <cbeutel@mymail.mines.edu>
2015-06-03 22:14:45 -0600

    Initial Commit

---
+++ StartFromScratch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartFromScratch
+//
+//  Created by Cara Beutel on 6/3/15.
+//  Copyright (c) 2015 Cara Beutel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
